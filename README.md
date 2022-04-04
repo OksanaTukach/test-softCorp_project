9b2cf90ca111e36e22dcf1398642ce5d64be88dc
OksTukach <Okstukach@gmail.com>
2022-04-04 11:00:29 +0300

    project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# test-softCorp_project
+# test_softCorp_project
+
+## Demo
+- [GitHub Pages](https://oksanatukach.github.io/test_softCorp/)
+
+## Install project for a local version
+
+```
+$ npm i
+$ npm start
+```
---
+++ dev/js/scripts.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+	const width = $(window).width();
+	const height = $(window).height();
+
+	console.log('Width: ' + width + 'px');
+	console.log('Height: ' + height + 'px');
+
+	headerMainScreen()
+
+	$('body').on('click', '.header__burger', function () {
+		$(this).toggleClass('header__burger_close');
+		$(this).siblings('.header__menu').toggleClass('header__menu_open')
+		headerMobHeight()
+	} )
+
+
+	var headerMobHeight = function headerMobHeight() {
+		var headerMob = document.querySelector('.header__menu');
+		var header = document.querySelector('.header');
+		setTimeout(function () {
+		  headerMob.style.height = window.innerHeight - header.clientHeight + 'px';
+		}, 1);
+	  };
+
+	
+	  
+	  (function() {
+
+		function scrollHorizontally(e) {
+			e = window.event || e;
+			var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+			document.querySelector('.order__points_scroll').scrollLeft -= (delta*10);
+			e.preventDefault();
+		}
+		if (document.querySelector('.order__points_scroll').addEventListener) {
+			document.querySelector('.order__points_scroll').addEventListener("mousewheel", scrollHorizontally, false);
+			document.querySelector('.order__points_scroll').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+		} else {
+			document.querySelector('.order__points_scroll').attachEvent("onmousewheel", scrollHorizontally);
+		}
+	
+	})();
+
+	$( ".slider" ).slider({
+		range: "max",
+		min: 1,
+		max: 100,
+		value: 75,
+		slide: function( event, ui ) {
+		  $( ".form__item_value .value" ).html( ui.value + '%'  );
+		}
+	});
+	$( ".form__item_value .value" ).html( $( ".slider" ).slider( "value" )+ '%' );
+	var lazyLoadInstance = new LazyLoad({
+		elements_selector: ".lazy",
+		load_delay: 100
+	});
+
+
+	if($('.styler').length){
+		$('.styler').styler();
+	}
+
+});
+
+
+$(window).resize(function () {
+
+	let width = $(window).width();
+	let height = $(window).height();
+	headerMainScreen()
+});
+
+
+var headerMainScreen = function headerMainScreen() {
+	var mainScreen = document.querySelector('.main-screen');
+	var mainScreenText = document.querySelector('.main-screen__information');
+	var header = document.querySelector('.header');
+	setTimeout(function () {
+		mainScreen.style.height = mainScreenText.clientHeight + header.clientHeight + 'px';
+	}, 100);
+};
